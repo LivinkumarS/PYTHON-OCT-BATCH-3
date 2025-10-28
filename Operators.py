52d828359b5d29420265a5bc9c71a13a45511782
LivinkumarS <001liv100@ggmail.com>
2025-10-28 12:41:12 +0530

    Opearators

---
+++ Operators.py
@@ -0,0 +1,54 @@
+# a=2
+# b=3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# print(2+3-6/2*4)
+
+# Comparison 
+# a=19
+# b=-10
+# print(a==b)
+# print(a!=b)
+
+# print(a>b)
+# print(a<b)
+
+# print(a<=b)
+# print(a>=b)
+
+
+# a=-45
+# b=-54
+
+# print(2<3 and 2>=2)
+# print(a or b)
+# print(not a)
+
+# print((a<b) and (b<a*b and (a*b//4) or (a%3*5<7**2)))
+
+# assignment operator
+# a=10
+# a=12
+# a+=18 
+# a-=20
+# a*=10
+# a/=5
+# a//=5
+# a%=4
+# a**99
+# print(a)
+
+# Bitwise Operators
+# print(9&10)
+# print(5|9)
+# print(5^9)
+# print(15>>3)
+# print(15<<3)
+print(~-1)
