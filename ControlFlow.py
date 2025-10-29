4f983fd71bb7041d313bc7774fd49386aa319995
LivinkumarS <001liv100@ggmail.com>
2025-10-29 12:25:19 +0530

    Control flow - conditional clause

---
+++ ControlFlow.py
@@ -0,0 +1,77 @@
+# print("Hello")
+# print("Hi")
+# print("Final")
+
+# inp=int(input("Enter your age: "))
+# if inp>=18:
+#     print("can vote")
+# else:
+#     print("cannot vote")
+
+
+# if a<b:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+    
+
+# print("Final step")
+# a=10
+# b=19
+
+# if a>b:
+#     print("From If Block!")
+# elif a==b: 
+#     print("From Elif1 Block!")
+# elif not a!=b: 
+#     print("From Elif2 Block!")
+# elif a<=b and a==b: 
+#     print("From Elif3 Block!")
+# else:
+#     print("From Else Block")
+    
+# if a+10>b:
+#     print("Hello")
+
+
+# mark
+
+# 91-100 --> O 
+# 81-90--> A+
+# 71-80-->A 
+# 61-70 --> B 
+# 50-60 -->C 
+# 0-49 --> Fail
+
+# marks = int (input("Enter your marks: "))
+# if marks >= 91 and marks <= 100:
+#     print("Grade: o")
+# elif marks >= 81 and marks <= 90:
+#     print("Grade: A+")
+# elif marks >= 71 and marks <= 80:
+#     print("Grade: A")
+# elif marks >= 61 and marks <= 70:
+#     print("Grade: B")
+# elif marks >= 50 and marks <= 60:
+#     print("Grade: C")
+# elif marks >= 0 and marks <= 50:
+#     print("Grade: fail")
+# else:
+#     print("invalid marks!")
+
+a=2
+
+if a>10:
+    if a>15:
+        print("very large")
+    else:
+        print("large")
+else:
+    if a<5:
+        print("very small")
+    else:
+        print("small")
+        
+print("Hello")
+
+print(123)
