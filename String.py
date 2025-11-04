a794c50f132eb97e0513a568d19593b04e325241
LivinkumarS <001liv100@ggmail.com>
2025-11-04 12:28:38 +0530

    Strings

---
+++ String.py
@@ -0,0 +1,50 @@
+# a="hello"
+# b="Hi"
+
+# print(a+b)
+# print(a*5)
+
+# print("hell" not in a)
+
+# name="Vijay"
+# age=51
+
+# print("His name is "+name+". He is "+str(age)+" years old!")
+
+# print(f"His name is {name}. He is {age} years old!")
+
+
+# His name is Vijay. He is 51 years old!
+
+# a="           hello world  "
+
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print(a.capitalize())
+
+# print(a)
+# print(a.strip())
+
+# print(str1.split(" "))
+
+# lis1=['Hello', 'guys', 'I', 'am', 'From', 'Tamilnadu']
+
+# print("---".join(lis1))
+
+# print(str1.find("you"))
+# print(str1.count("a"))
+
+# str1="HelloguysIamFromTamilnadu"
+# str2="123^"
+
+# print(str1.isalpha())
+# print(str2.isdigit())
+
+# Palindrome
+
+a = input("Enter a word or number: ")
+if a == a[::-1]:
+    print("It is a palindrome")
+else:
+    print("It is not a palindrome")
